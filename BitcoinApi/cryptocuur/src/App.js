08909a6e78f9a318b32186a8d8f38be7bf6526a3
unknown <sarveshsamant1140@gmail.com>
2021-01-14 02:07:28 +0530

    without CSS

---
+++ BitcoinApi/cryptocuur/src/App.js
@@ -0,0 +1,89 @@
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Coin from "./Components/Coin";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => setData(response.data))
+      .catch((err) => setData(err));
+  }, []);
+  const handleChange = (e) => {
+    setSearch(e);
+  };
+  console.log(data);
+
+  const filteredData = data.filter((coin) => {
+    return coin.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+  console.log(filteredData);
+  return (
+    <main className="app">
+      {data && (
+        <>
+          <div className="coin-search">
+            <h1 className="coin-text">Search Currency</h1>
+            <form>
+              <input
+                type="text"
+                placeholder="Search"
+                className="coin-input"
+                value={search}
+                onChange={(e) => handleChange(e.target.value)}
+              />
+            </form>
+          </div>
+          {filteredData.map((coin) => {
+            // console.log(coin.price_change_percentage_24h);
+            return (
+              <Coin
+                key={coin.id}
+                image={coin.image}
+                name={coin.name}
+                symbol={coin.symbol}
+                volume={coin.market_cap}
+                priceChange={coin.price_change_percentage_24h}
+                marketCap = {coin.total_volume}
+              />
+            );
+          })}
+        </>
+      )}
+    </main>
+  );
+}
+
+export default App;
+
+// ath: 3073058
+// ath_change_percentage: -14.0592
+// ath_date: "2021-01-08T15:05:37.863Z"
+// atl: 3993.42
+// atl_change_percentage: 66034.05812
+// atl_date: "2013-07-05T00:00:00.000Z"
+// circulating_supply: 18599543
+// current_price: 2663841
+// fully_diluted_valuation: 55542932381100
+// high_24h: 2644902
+// id: "bitcoin"
+// // image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+// last_updated: "2021-01-13T20:15:24.944Z"
+// low_24h: 2386377
+// market_cap: 49193959960399
+// market_cap_change_24h: 2083307564263
+// market_cap_change_percentage_24h: 4.42216
+// market_cap_rank: 1
+// max_supply: 21000000
+// name: "Bitcoin"
+// price_change_24h: 155395
+// price_change_percentage_24h: 6.19487
+// roi: null
+// symbol: "btc"
+// total_supply: 21000000
+// total_volume: 4630601360082
---
+++ BitcoinApi/cryptocuur/src/Components/Coin.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Coin.css";
+const Coin = ({
+  name,
+  image,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketCap,
+}) => {
+  if (priceChange === null) {
+    priceChange = 0;
+  }
+  return (
+    <>
+      {name && (
+        <>
+          <div className="coin-container">
+            <div className="coin-row">
+              <div className="coin">
+                <img src={image} alt="" />
+                <h1>{name}</h1>
+                <p className="coin-symbol">{symbol}</p>
+              </div>
+              <div className="coin-data">
+                <p className="coin-price">{price}</p>
+                <p className="coin-volume">{volume.toLocaleString()}</p>
+                {priceChange < 0 ? (
+                  <p className="coin-percent red">{priceChange.toFixed(3)}</p>
+                ) : (
+                  <p className="coin-percent green">{priceChange.toFixed(3)}</p>
+                )}
+                <p className="coin-marketcap">
+                  MKT CAP : ${marketCap.toLocaleString()}
+                </p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Coin;
